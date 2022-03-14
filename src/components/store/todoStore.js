905f0a123f353f93ac3f44433c91b59d59e42154
Amrita Matta <matta.amrita@gmail.com>
2022-03-14 14:46:42 +0200

    final solution with strikeout toggle functionality

--- src/components/store/todoStore.js
+++ src/components/store/todoStore.js
@@ -1,75 +1,41 @@
-import React, { useReducer } from "react";
+import React, { useContext } from "react";
 
-const initialState = {
-  notes: [
-    {
-      id: 1,
-      title: "Create clean app",
-      task: "npx create-react-app",
-      done: false,
-    },
-    {
-      id: 2,
-      title: "Clean app",
-      task: "Delete and clean unnecessary stuff",
-      done: false,
-    },
-    {
-      id: 3,
-      title: "Create store / context",
-      task: "Create new file and use React.createContext()",
-      done: false,
-    },
-  ],
-};
+import classes from "./TodoList.module.css";
+import { NotesContext } from "../store/todoStore";
 
-export const NotesContext = React.createContext();
+const TodoList = () => {
+  const ctx = useContext(NotesContext);
 
-const reducer = (state, action) => {
-  switch (action.type) {
-    case "ADD_NOTE":
-      return {
-        notes: [
-          ...state.notes,
-          {
-            id: new Date().valueOf(),
-            title: action.todo.title,
-            task: action.todo.task,
-            done: false,
-          },
-        ],
-      };
-    case "DONE_NOTE":
-      return;
-    default:
-      return state;
-  }
-};
-
-export const Provider = ({ children }) => {
-  const [state, dispatch] = useReducer(reducer, initialState);
-
-  const addTodoItem = (todo) => {
-    dispatch({
-      type: "ADD_NOTE",
-      todo: todo,
-    });
-  };
-
-  const doneTodo = (todo) => {
-    dispatch({
-      type: "DONE_NOTE",
-      todo: todo,
-    });
-  };
-
-  const value = {
-    notes: state.notes,
-    addTodoItem: addTodoItem,
-    doneTodo: doneTodo,
+  const removeHandler = (id) => {
+    ctx.removeTodo(id);
   };
 
   return (
-    <NotesContext.Provider value={value}>{children}</NotesContext.Provider>
+    <div className={classes.todos}>
+      <h1>Notes:</h1>
+      {ctx.notes.map((note) => {
+        return (
+          <div
+            className={`${classes.todo} ${
+              note.done ? classes.done : classes.notDone
+            }`}
+            key={note.id}
+            onClick={() => ctx.doneTodo(note.id)}
+          >
+            <h2>{note.title}</h2>
+            <p>{note.task}</p>
+            <p></p>
+            <span
+              className={`material-icons ${classes.delete}`}
+              onClick={() => removeHandler(note.id)}
+            >
+              delete
+            </span>
+          </div>
+        );
+      })}
+    </div>
   );
 };
+
+export default TodoList;
