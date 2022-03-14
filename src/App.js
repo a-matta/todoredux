b9107c491be85ab1828375b4e177770a1239247d
Amrita Matta <matta.amrita@gmail.com>
2022-03-14 14:42:32 +0200

    updated store

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import "./App.css";
 import React from "react";
 import AddTodo from "./components/Todo/AddTodo";
 import TodoList from "./components/Todo/TodoList";
+import { Provider } from "./components/store/todoStore";
 
-function App() {
+const App = () => {
   return (
     <div>
+    <Provider>
       <AddTodo />
       <TodoList />
     </div>
+    </Provider>
   );
 }
 
-export default App;
